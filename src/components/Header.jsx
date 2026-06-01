6ae8a2eb69fca51b1fb3ce82954a3c50dd25cd0a
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-01 19:37:25 +0530

    feat: add Bootstrap integration and new components (Header, Footer, Counter)

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <>
+            <div className="bg-primary-subtle p-2 d-flex justify-content-center align-items-center">
+                <div className="text-primary fs-4 fw-bold">My Comapany</div>
+                <div></div>
+
+            </div>
+        </>
+    )
+}
+
+export default Header;
