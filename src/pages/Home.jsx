35d3901c0b852e8056dc1ff5b683dcc23b946fca
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-03 19:42:22 +0530

    update Home component to include additional heading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ function Home (){
     return (
         <>
             <div>
-                <h1>Home</h1>
+                <h1>Home </h1>
+                <h1>Home 1</h1>
             </div>
         </>
     )
