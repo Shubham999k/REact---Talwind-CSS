1cc9eabb07c7ff36af600e15f5e9ef3978cd283e
Shubham999999999999 <shubhamchaudharik485@gmail.com>
2026-06-06 20:09:36 +0530

    style: update Header component navbar styles for improved visibility and consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 function Header() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary py-1">
+            <nav className="navbar navbar-expand-lg navbar-primary bg-primary-subtle py-1 sticky-top">
                 <div className="container">
 
                     {/* Logo */}
@@ -13,34 +13,34 @@ function Header() {
 
                     {/* Center Menu */}
                     <div className="mx-auto">
-                        <ul className="navbar-nav flex-row gap-4">
+                        <ul className="navbar-nav flex-row gap-4 ">
 
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to="/">
+                                <NavLink className="nav-link text-primary" to="/">
                                     Home
                                 </NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to="/about">
+                                <NavLink className="nav-link text-primary" to="/about">
                                     About
                                 </NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to="/contact">
+                                <NavLink className="nav-link text-primary" to="/contact">
                                     Contact
                                 </NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to="/product">
+                                <NavLink className="nav-link text-primary" to="/product">
                                     Product
                                 </NavLink>
                             </li>
 
                             <li className="nav-item">
-                                <NavLink className="nav-link text-white" to="/counter">
+                                <NavLink className="nav-link text-primary" to="/counter">
                                     Counter
                                 </NavLink>
                             </li>
@@ -52,14 +52,14 @@ function Header() {
                     <div className="d-flex gap-2">
 
                         <NavLink
-                            className="btn btn-light text-primary fw-semibold"
+                            className="btn btn-outline-primary fw-semibold"
                             to="/login"
                         >
                             Login
                         </NavLink>
 
                         <NavLink
-                            className="btn btn-light text-primary fw-semibold"
+                            className="btn btn-primary fw-semibold"
                             to="/register"
                         >
                             Register
